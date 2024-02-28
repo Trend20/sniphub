8051d8eaf3876fb3f0599d5175d80e5145a1eafe
Trend20 <enockomondi305@gmail.com>
2024-02-28 19:36:44 +0300

    adding a header

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Snippets",
+    url: "/snippets",
+  },
+  {
+    id: 3,
+    name: "About",
+    url: "/about",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex items-center justify-between w-full px-36 py-10">
+      <div className="flex">
+        <h1>Sniphub</h1>
+      </div>
+      <div className="flex w-96 justify-center">
+        {navLinks.map((item) => (
+          <Link href={item.url} key={item.id} className="flex capitalize px-5">
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      <div className="flex">
+        <button>New Snippet</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
