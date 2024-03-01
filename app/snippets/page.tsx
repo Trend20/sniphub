f7b3ff72b7db2323bc22cb0b639edcec8b33ac58
Trend20 <enockomondi305@gmail.com>
2024-03-01 19:13:32 +0300

    adding snippet editors

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -2,10 +2,19 @@
 import AddSnippetDialog from "@/components/AddSnippetDialog";
 import React from "react";
 import { BsFileEarmarkCodeFill } from "react-icons/bs";
+import {
+  Collapse,
+  Button,
+  Card,
+  Typography,
+  CardBody,
+} from "@material-tailwind/react";
 
 const Snippets = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen((cur) => !cur);
+  const [showSnippet, setShowSnippet] = React.useState(false);
+  const toggleOpen = () => setShowSnippet((cur) => !cur);
   return (
     <div className="flex flex-col px-36 w-3/4 m-auto mt-30 border border-grey rounded-md p-3">
       <div className="flex items-center justify-between">
@@ -26,13 +35,30 @@ const Snippets = () => {
             element you want to apply dark mode styles to.
           </p>
           <div className="flex justify-end items-center">
-            <button className="flex items-center p-2 bg-meta-4 rounded-md text-whiten">
+            <button
+              onClick={toggleOpen}
+              className="flex items-center p-2 bg-meta-4 rounded-md text-whiten"
+            >
               <i className="mr-3">
                 <BsFileEarmarkCodeFill />
               </i>
               Show Snippet
             </button>
           </div>
+          {/* code snippet */}
+          {showSnippet && (
+            <Collapse open={showSnippet}>
+              <Card className="my-4 mx-auto w-8/12">
+                <CardBody>
+                  <Typography>
+                    Use our Tailwind CSS collapse for your website. You can use
+                    if for accordion, collapsible items and much more.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </Collapse>
+          )}
+          {/* code snippet */}
         </div>
         <div className="flex flex-col border border-grey rounded-md p-3">
           <h5 className="text-xl font-bold">How to add dark theme!</h5>
